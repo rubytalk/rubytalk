b9ec5e7327ff52092d493b647f349d565965d9c7
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2015-02-20 17:54:08 -0200

    Add initial implementation for slack invitation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,5 @@
 class MembersController < ApplicationController
+  ENDPOINT = "https://rubytalk.slack.com/api/users.admin.invite"
   def new
     @member = Member.new
   end
@@ -7,6 +8,7 @@ class MembersController < ApplicationController
     @member = Member.new(member_params)
 
     if @member.save
+      invite_to_slack
       redirect_to new_member_url, notice: 'Member was successfully created.'
     else
       render :new
@@ -17,5 +19,16 @@ class MembersController < ApplicationController
 
   def member_params
     params.require(:member).permit(:name, :email)
+  end
+
+  def invite_to_slack
+    RestClient.post ENDPOINT,
+      't'           => Time.now.to_i,
+      'channels'    => ENV['CHANNELS'],
+      'email'       => @member.email,
+      'first_name'  => @member.name,
+      'token'       => ENV['API'],
+      'set_active'  => true,
+      '_attempts'   => 1
   end
 end
